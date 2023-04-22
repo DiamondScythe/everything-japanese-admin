dc41b4d73a4f70a4064d38ba23161e350c709eb8
DiamondScythe <nhutoan399@gmail.com>
2023-04-22 15:50:59 +0700

    added manage view

---
+++ src/components/AddGrammarForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>This is the form for add new grammar points to the database</h1>
+    <!-- form with inputs for title, explanation and example sentence text fields-->
+    <form>
+      <label for="title">Title</label>
+      <input type="text" id="title" name="title" />
+      <label for="explanation">Explanation</label>
+      <input type="text" id="explanation" name="explanation" />
+      <label for="example">Example</label>
+      <input type="text" id="example" name="example" />
+      <input type="submit" value="Submit" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddGrammarForm",
+  props: {},
+};
+</script>
---
+++ src/views/ManageView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>This is the page for managing stuff</h1>
+    <AddGrammarForm />
+  </div>
+</template>
+
+<script>
+import AddGrammarForm from "@/components/AddGrammarForm.vue";
+export default {
+  name: "ManageView",
+  components: { AddGrammarForm },
+};
+</script>
