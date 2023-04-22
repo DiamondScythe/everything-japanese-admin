37439ab81d542ee0c9535cadbc2616912585ce56
DiamondScythe <nhutoan399@gmail.com>
2023-04-22 23:11:44 +0700

    added grammar form

--- src/components/AddGrammarForm.vue
+++ src/components/AddGrammarForm.vue
@@ -1,22 +1,93 @@
 <template>
-  <div class="about">
-    <h1>This is the form for add new grammar points to the database</h1>
-    <!-- form with inputs for title, explanation and example sentence text fields-->
-    <form>
-      <label for="title">Title</label>
-      <input type="text" id="title" name="title" />
-      <label for="explanation">Explanation</label>
-      <input type="text" id="explanation" name="explanation" />
-      <label for="example">Example</label>
-      <input type="text" id="example" name="example" />
-      <input type="submit" value="Submit" />
-    </form>
-  </div>
+  <v-container>
+    <v-card>
+      <v-card-title> Add Grammar Lesson </v-card-title>
+      <v-card-text>
+        <v-form v-model="valid" @submit.prevent="submitForm">
+          <v-text-field
+            v-model="title"
+            :rules="[rules.required]"
+            label="Title"
+          ></v-text-field>
+          <v-textarea
+            v-model="explanation"
+            :rules="[rules.required]"
+            label="Explanation"
+          ></v-textarea>
+          <v-card
+            v-for="(example, index) in examples"
+            :key="index"
+            class="my-3"
+          >
+            <v-card-title> Example {{ index + 1 }} </v-card-title>
+            <v-card-text>
+              <v-text-field
+                v-model="example.example"
+                :rules="[rules.required]"
+                label="Example"
+              ></v-text-field>
+              <v-text-field
+                v-model="example.translation"
+                :rules="[rules.required]"
+                label="Translation"
+              ></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="red" @click="removeExample(index)"
+                >Remove Example</v-btn
+              >
+            </v-card-actions>
+          </v-card>
+          <v-btn text color="primary" @click="addExample">Add Example</v-btn>
+          <v-btn :disabled="!valid" color="primary" type="submit"
+            >Save Grammar Lesson</v-btn
+          >
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
+  data() {
+    return {
+      title: "",
+      explanation: "",
+      examples: [{ example: "", translation: "" }],
+      rules: {
+        required: (value) => !!value || "Required.",
+      },
+      valid: false,
+    };
+  },
   name: "AddGrammarForm",
   props: {},
+  methods: {
+    addExample() {
+      this.examples.push({ example: "", translation: "" });
+    },
+    removeExample(index) {
+      this.examples.splice(index, 1);
+    },
+    submitForm() {
+      console.log(this.title, this.explanation, this.examples);
+      //post to localhost 8081 with axios
+      axios
+        .post("http://localhost:8081/oneGrammar", {
+          title: this.title,
+          explanation: this.explanation,
+          examples: this.examples,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
---
+++ src/components/nhap.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title> Add grammar point </v-card-title>
+      <v-card-text>
+        <v-form v-model="valid">
+          <v-text-field
+            v-model="name"
+            :rules="[rules.required]"
+            label="Name"
+          ></v-text-field>
+          <v-text-field
+            v-model="email"
+            :rules="[rules.required, rules.email]"
+            label="Email"
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            :rules="[rules.required, rules.min(6)]"
+            label="Password"
+          ></v-text-field>
+          <v-btn :disabled="!valid" @click="submitForm">Submit</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      skills: [],
+      valid: false,
+      rules: {
+        required: (value) => !!value || "Required.",
+        email: (value) =>
+          /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(value) ||
+          "Invalid Email Address.",
+        min: (value) => (value && value.length >= 6) || "Min 6 characters.",
+      },
+    };
+  },
+  name: "nhap",
+  props: {},
+  methods: {
+    submitForm() {
+      console.log(this.name, this.email, this.password);
+    },
+    methods: {
+      addSkill() {
+        this.skills.push("");
+      },
+      submitForm() {
+        console.log(this.name, this.email, this.skills);
+      },
+    },
+  },
+};
+</script>
